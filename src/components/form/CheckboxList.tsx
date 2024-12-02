160bb1ab1bbc32d81b57f040e846cac9f60ed69b
Bence Tóth <me@bnctth.dev>
2024-12-02 23:24:02 +0100

    add for for checkbox label

--- src/components/form/CheckboxList.tsx
+++ src/components/form/CheckboxList.tsx
@@ -9,10 +9,10 @@ const CheckboxList = ({values, setValues, names}: CheckboxListProps) => {
         <div>
             {[...Array(names.length)].map((_, i) =>
                 <div key={names[i]} className="flex justify-start items-center gap-2">
-                    <input className="w-4 h-4" checked={values[i]}
+                    <input id={`chkbox-${i}`} className="w-4 h-4" checked={values[i]}
                            onChange={e => setValues([...values.slice(0, i), e.target.checked, ...values.slice(i + 1)])}
                            type="checkbox"/>
-                    <label>{names[i]}</label>
+                    <label htmlFor={`chkbox-${i}`}>{names[i]}</label>
                 </div>
             )}
         </div>
